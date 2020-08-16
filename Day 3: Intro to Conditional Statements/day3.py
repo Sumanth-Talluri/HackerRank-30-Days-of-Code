f941390e7f5993f8624818160d8708b40b50f275
SumanthTalluri <tallurisaisumanth77@gmail.com>
2020-08-16 13:30:08 +0530

    Create day3.py

---
+++ Day 3: Intro to Conditional Statements/day3.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+    if N % 2 == 0:
+        if (N>=2 and N<=5) or N>20:
+            print("Not Weird")
+        elif N>=6 and N<=20:
+            print("Weird")
+    else:
+        print("Weird")
